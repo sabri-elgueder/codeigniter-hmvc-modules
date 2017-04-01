f3a8ccb4c23e6f4db8453ea9bb0c4fb09d0b56e5
sabri <sabri.elgueder@satoripop.com>
2017-04-02 01:15:50 +0200

    First commit

---
+++ core/SP_Loader.php
@@ -0,0 +1,8 @@
+<?php
+(defined('BASEPATH')) or exit('No direct script access allowed');
+
+/* load the HMVC_Loader class */
+require APPPATH . 'third_party/HMVC/Loader.php';
+
+class SP_Loader extends HMVC_Loader {
+}
---
+++ core/SP_Router.php
@@ -0,0 +1,8 @@
+<?php
+(defined('BASEPATH')) or exit('No direct script access allowed');
+
+/* load the HMVC_Router class */
+require APPPATH . 'third_party/HMVC/Router.php';
+
+class SP_Router extends HMVC_Router {
+}
